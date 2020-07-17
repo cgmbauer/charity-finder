ff782fc2422fa1673006144bea07da14b3c72e7d
cgmbauer <cgmbauer@gmail.com>
2020-07-17 13:34:29 -0300

    removed MouseUp component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, MouseEvent } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -23,7 +23,7 @@ const Header: React.FC = () => {
       setOpen(false);
     }
   }
-  // && !menuBlock.current.contains(e.target as Node)
+
   useEffect(() => {
     document.addEventListener('mouseup', handleClickOutside);
     return () => {
