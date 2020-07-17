15d385bb2d5381291f27ca93a1d71650e700e350
cgmbauer <cgmbauer@gmail.com>
2020-07-17 13:33:43 -0300

    added h3 and select styles

---
+++ src/pages/findyourcharity/styles.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  text-align: center;
+
+  padding-top: 85px;
+
+  h3 {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    font-size: 47px;
+
+    margin-bottom: 0;
+    padding-bottom: 0;
+  }
+`;
+
+export const HorizontalLine = styled.div`
+  background-color: #000;
+
+  height: 1px;
+  width: 230px;
+
+  padding: 0;
+  margin: 0 auto 60px auto;
+`;
+
+export const SelectContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  select {
+    width: 235px;
+
+    text-align-last: center;
+    font-family: 'Poppins', 'sans-serif';
+    font-weight: 400;
+    font-style: italic;
+
+    background-color: #fff;
+
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+
+    & + select {
+      margin-left: 35px;
+    }
+
+    option {
+      text-align-last: center;
+      text-align: center;
+      font-family: 'Poppins', 'sans-serif';
+      font-weight: 400;
+      font-style: italic;
+
+      background-color: #fff;
+    }
+  }
+  option {
+    text-align-last: center;
+    text-align: center;
+    font-family: 'Poppins', 'sans-serif';
+    font-weight: 400;
+    font-style: italic;
+
+    background-color: #fff;
+  }
+`;
