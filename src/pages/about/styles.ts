3a437fdb17df56590a42648b5c1459b2c3cebd43
cgmbauer <cgmbauer@gmail.com>
2020-07-19 23:45:36 -0300

    GlobalGiving Container styled

--- src/pages/about/styles.ts
+++ src/pages/about/styles.ts
@@ -5,8 +5,7 @@ export const AboutContainer = styled.div`
 
   padding-top: 85px;
   margin-top: 30px;
-
-  height: 100vh;
+  margin-bottom: 0px;
 
   h3 {
     font-family: 'Roboto', sans-serif;
@@ -19,11 +18,34 @@ export const AboutContainer = styled.div`
   }
 
   p {
+    max-width: 890px;
+    margin: 0 auto;
+
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    font-style: italic;
+
     span {
       display: block;
 
-      margin-top: 23px;
+      margin-top: 7px;
     }
+  }
+
+  a {
+    display: inline-block;
+
+    text-decoration: none;
+
+    border-radius: 5px;
+
+    margin-top: 15px;
+    padding: 10px 30px;
+
+    background-color: #ff6f00;
+    color: #fff;
+    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.75);
   }
 `;
 
@@ -35,4 +57,42 @@ export const HorizontalLine = styled.div`
 
   padding: 0;
   margin: 0 auto 40px auto;
+`;
+
+export const GlobalGivingContainer = styled.div`
+  text-align: center;
+
+  margin-top: 65px;
+
+  h3 {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    font-size: 47px;
+
+    margin-bottom: 0;
+    margin-top: 20px;
+    padding-bottom: 0;
+  }
+
+  p {
+    max-width: 890px;
+
+    margin: 0 auto;
+
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    font-style: italic;
+    line-height: 30px;
+
+    & + p {
+      margin-top: 15px;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: #000000;
+    font-weight: 700;
+  }
 `;
