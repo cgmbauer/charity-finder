deb97dc206af67c088b3b236cca3a1ef3b23df76
cgmbauer <cgmbauer@gmail.com>
2020-07-19 22:46:18 -0300

    section about structured

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AboutContainer, HorizontalLine } from './styles';
 
 import Header from '../../components/header';
 
@@ -6,6 +7,21 @@ const About: React.FC = () => {
   return (
     <>
       <Header />
+      <AboutContainer>
+        <h3>ABOUT</h3>
+        <HorizontalLine />
+        <p>
+          Sometimes can be hard to find a project that you can trust and wants
+          to support, right?
+          <span />
+          We’ll help you to find that perfect charity project that you were
+          looking for all this time, even if it is on the other side of the
+          world!
+          <span />
+          Let’s get started?
+        </p>
+        <Link to="/">LET'S GO!</Link>
+      </AboutContainer>
     </>
   );
 };
